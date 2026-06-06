8477ebd675c3ec2657afea3efc741031f913254d
cmyk-code-sudo <wwwbibiqqcom@qq.com>
2026-06-06 13:39:06 +0800

    Add Vue.js frontend entry point

---
+++ src/frontend/main.js
@@ -0,0 +1,15 @@
+/**
+ * Research Browser Frontend - Entry Point
+ */
+
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+const app = createApp(App);
+
+app.use(store);
+app.use(router);
+
+app.mount('#app');
