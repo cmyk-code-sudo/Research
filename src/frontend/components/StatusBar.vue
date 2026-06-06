9ba3ae54defefd72b0a2ba5b7bdd30ef3acb68b6
cmyk-code-sudo <wwwbibiqqcom@qq.com>
2026-06-06 13:38:25 +0800

    Add StatusBar Vue component

---
+++ src/frontend/components/StatusBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer class="status-bar">
+    <span class="status-text">{{ statusMessage }}</span>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'StatusBar',
+  data() {
+    return {
+      statusMessage: 'Ready',
+    };
+  },
+};
+</script>
+
+<style scoped>
+.status-bar {
+  display: flex;
+  align-items: center;
+  padding: 5px 15px;
+  background-color: #f5f5f5;
+  border-top: 1px solid #ddd;
+  height: 25px;
+  font-size: 12px;
+  color: #666;
+}
+
+.status-text {
+  flex: 1;
+}
+</style>
